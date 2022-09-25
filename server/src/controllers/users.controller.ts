63cffdc659620948a9025fff4acbbdc48a65a69c
Lê Công Lý <58856636+lecongly@users.noreply.github.com>
2022-09-25 22:34:57 +0700

    feat (server): activate email account

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
 
 import { validateEmail } from "../validations";
 import { createActivationToken } from "../services/jwt";
 import sendMail from "../services/sendMail";
 
 import Users from "../models/users.model";
-import { CLIENT_URL } from "../config";
+import { ACTIVATION_TOKEN_SECRET, CLIENT_URL } from "../config";
+import { UserJwtPayload } from "../types/jwt.type";
 
 const userController = {
   register: async (req: Request, res: Response) => {
@@ -43,6 +45,31 @@ const userController = {
       });
     } catch (error: any) {
       return res.status(500).json({ msg: error.message });
+    }
+  },
+  activateEmail: async (req: Request, res: Response) => {
+    const { activation_token } = req.body;
+    try {
+      const user = <UserJwtPayload>(
+        jwt.verify(activation_token, ACTIVATION_TOKEN_SECRET)
+      );
+      const { name, email, password } = user;
+
+      const check = await Users.findOne({ email });
+      if (check)
+        return res.status(400).json({ msg: "This email already exists." });
+
+      const newUser = new Users({
+        name,
+        email,
+        password,
+      });
+
+      await newUser.save();
+
+      res.json({ msg: "Account has been activated!" });
+    } catch (err: any) {
+      return res.status(500).json({ msg: err.message });
     }
   },
 };
--- server/src/routes/api/users.routes.ts
+++ server/src/routes/api/users.routes.ts
@@ -3,5 +3,6 @@ import usersController from "../../controllers/users.controller";
 
 const route = Router();
 route.post("/register", usersController.register);
+route.post("/activation", usersController.activateEmail);
 
 export default route;
