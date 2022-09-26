63421637ccd3966d9bd5e09816dd160d8d2cf57e
Lê Công Lý <58856636+lecongly@users.noreply.github.com>
2022-09-26 23:11:02 +0700

    feat (server): upload image

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import usersRoute from "./api/users.routes";
+import uploadRoute from "./api/upload.routes";
 
 const route = Router();
 route.use("/users", usersRoute);
+route.use("/upload", uploadRoute);
 
 export default route;
